ae60f2687087155861347095f96d46e585ef7ef8
patrik persson <patrik.persson10@tele2.se>
2016-07-29 18:30:13 +0200

    Update Contacts and Invitation with a Delete button

--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -3,7 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
 	model() {
-    return this.store.findAll('invitation');
-  }
+    	return this.store.findAll('invitation');
+  	},
+
+  	actions: {
+
+    	deleteInvitation(invitation) {
+      		let confirmation = confirm('Are you sure?');
+
+      		if (confirmation) {
+        		invitation.destroyRecord();
+      		}
+    	}
+  	}
   
 });
